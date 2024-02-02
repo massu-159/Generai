6d7c604cef90592333c376efac7d1f06c915ad5b
massu-159 <magjtwjp@gmail.com>
2024-02-03 07:47:16 +0900

    サイドバー・ヘッダーを追加

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+
+const DashboardPage=()=> {
+  return (
+    <div>
+      <p>Dashboard page(Protected)</p>
+    </div>
+  );
+}
+
+export default DashboardPage
